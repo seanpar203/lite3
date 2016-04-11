5b6fb802b32a4ecf84137fd6d574b842ec9a8377
Sean Parsons <sean@labelinteractive.com>
2016-04-11 14:44:37 -0400

    Added .table().update() test

--- test/index.test.js
+++ test/index.test.js
@@ -9,19 +9,29 @@ const assert = chai.assert;
 
 // Library
 const lite = require('./../index');
+let lite3 = new lite('test.db');
 
 
 describe('lite3-tests', function () {
 
 	// table()
 	describe('table()', function () {
-		it('should set the tableName property', function () {
-			let lite3 = new lite('test.db');
+		it('should set the lite3.tableName property', function () {
 			lite3.table('people');
 			expect(lite3.tableName).to.include('people');
 		});
 	});
 
+	// table().update()
+	describe('table().update()', function () {
+		it('should set lite3.stmt, lite3.queryStmt and lite3.queryType', function () {
+			lite3.table('people').update('(?,?,?)');
+			expect(lite3.tableName).to.equal('people');
+			expect(lite3.queryStmt).to.equal('(?,?,?)');
+			expect(lite3.queryType).to.equal('UPDATE');
+		});
+	});
+	
 	
 
 });
